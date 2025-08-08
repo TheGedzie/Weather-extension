121119cfb82cc4bf81d6fb7a195848d7c7caa0bb
TheGedzie <gedzie06@gmail.com>
2025-08-08 20:54:25 +0700

    Realise version: v 1.0

---
+++ public/ObjState.js
@@ -0,0 +1,18 @@
+export const State = {
+    'ясно': './assets/videos/sun.gif',
+    'малооблачно': './assets/videos/sun.gif',
+    'дождь': './assets/videos/rain.gif',
+    'небольшой дождь': './assets/videos/rain.gif',
+    'сильный дождь': './assets/videos/rain.gif',
+    'ливень': './assets/videos/rain.gif',
+    'морось': './assets/videos/rain.gif',
+    'снег': './assets/videos/cold.gif',
+    'метель': './assets/videos/cold.gif',
+    'град': './assets/videos/cold.gif',
+    'гроза': './assets/videos/storm.gif',
+    'туман': './assets/videos/storm.gif',
+    'переменная облачность': './assets/videos/cloudy.gif',
+    'облачно с прояснениями': './assets/videos/cloudy.gif',
+    'облачно': './assets/videos/cloudy.gif',
+    'пасмурно': './assets/videos/cloudy.gif'
+}
---
+++ public/script.js
@@ -0,0 +1,59 @@
+import {API_KEY} from './API_KEY.js'
+import {State} from './ObjState.js'
+const weather = document.querySelector('[data-weather-info]')
+
+document.querySelector('[data-weather-btn]').addEventListener('click', () => {
+    const city = document.querySelector('[data-city-value]').value
+
+    if(city){
+        renderWeather(city)
+    }
+    else{
+        alert('Сначала введите город')
+    }
+})
+
+const renderWeather = async(city) =>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=ru`
+    
+    try{
+        const response = await fetch(url)
+        if(!response.ok) throw new Error(`Ошибка сервера: ${response.status}`);
+
+        const data = await response.json()
+
+        if(!weather.classList.contains('weather__info')){
+            weather.classList.add('weather__info')
+        }
+        
+        let gif_url = null
+        Object.keys(State).forEach(key => {
+
+            if(key == data.weather[0].description){
+                gif_url = State[key]
+            }
+        });
+        weather.innerHTML = 
+        `
+            <div class="weather__info-gif">
+            <img src="${gif_url}" alt="Weather gif">
+            </div>
+            <h1 class="weather__info-city">${data.name}</h1>
+            <h2 class="weather__info-state">${data.weather[0].description}</h2>
+            <div class="weather__info-wind-container">
+                <p class="weather__info-wind">Ветер: ${data.wind.speed}  м/c</p>
+                <img src="./assets/videos/wind.gif" alt="Wind gif">
+            </div>
+            <p class="weather__info-temperature">Температура : ${data.main.temp} градусов C</p>
+        `
+    }
+    catch(error){
+        weather.innerHTML = 
+        `
+            <p style = 'color:red'}>Непредвиденная ошибка: ${error.message}</p>
+
+        `
+    }
+
+
+}
